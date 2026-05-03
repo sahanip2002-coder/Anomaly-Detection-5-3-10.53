6e2e012aed4227b36168b940e58c5b78a8b3a812
SahaniPerera <sahanip2002@gmail.com>
2026-05-03 14:46:07 +0530

    Delete server directory

--- server/app/state.py
+++
@@ -1,8 +0,0 @@
-# This file holds the in-memory database so all modules share the SAME data
-devices = {}
-ota_log = []
-anomaly_count = 0
-
-def increment_anomaly():
-    global anomaly_count
-    anomaly_count += 1
